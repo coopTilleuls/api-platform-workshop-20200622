20b27518e055538c7bd65d9f8f244a55fc9cc6ad
Kévin Dunglas <dunglas@gmail.com>
2020-07-22 10:50:09 +0200

    feat: use Doctrine for the product API

--- api/src/Entity/Product.php
+++ api/src/Entity/Product.php
@@ -6,28 +6,34 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
 
 /**
  * A product to sell on our website.
  *
  * @ApiResource
+ * @ORM\Entity
  */
 class Product
 {
     /**
-     * @var int The identifier of the product.
-     *
-     * @ApiProperty(identifier=true)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
      */
     public ?int $id = null;
 
     /**
      * @var string The name of the product
+     *
+     * @ORM\Column
      */
-    public string $name;
+    public string $name = '';
 
     /**
      * @var string The price of the product.
+     *
+     * @ORM\Column(type="decimal", scale=2, precision=10)
      */
-    public string $price;
+    public string $price = '';
 }
